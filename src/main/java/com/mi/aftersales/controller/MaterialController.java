57abfd91050f7ae9413f8516d45c9949dc2d5d3f
QYenon <hello@QYenon.com>
2024-05-21 21:11:54 +0800

    库管添加物料和更新物料信息接口

--- src/main/java/com/mi/aftersales/controller/MaterialController.java
+++ src/main/java/com/mi/aftersales/controller/MaterialController.java
@@ -1,7 +1,24 @@
 package com.mi.aftersales.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.convert.ConvertException;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.feiniaojin.gracefulresponse.GracefulResponseException;
+import com.mi.aftersales.entity.Material;
+import com.mi.aftersales.exception.graceful.ServerErrorException;
+import com.mi.aftersales.service.IMaterialService;
+import com.mi.aftersales.vo.form.ManngerAddMaterialForm;
+import com.mi.aftersales.vo.form.ManngerUpdateMaterialForm;
+import io.swagger.v3.oas.annotations.Operation;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
 
 /**
  * <p>
@@ -15,4 +32,106 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/aftersales/material")
 public class MaterialController {
 
+    private static final String NAMESPACE_4_ORDER_LOCK = "materialLock:";
+
+    @Resource
+    private IMaterialService imaterialService;
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    @PostMapping(path = "/materialManager/add")
+    @Operation(summary = "库管添加物料", description = "库管添加物料")
+    public void addMaterial(@RequestBody @Valid ManngerAddMaterialForm form) {
+
+        LambdaQueryWrapper<Material> queryWrapper = new LambdaQueryWrapper<>();
+
+        queryWrapper.eq(null != form.getMaterialName(), Material::getMaterialName, form.getMaterialName());
+
+        List<Material> materialList = imaterialService.list(queryWrapper);
+
+        if (null != materialList && materialList.size() > 0) {
+            throw new GracefulResponseException("物料命名重复，请更换物料名!");
+        }
+
+        if (form.getAlertNum().compareTo(form.getStock()) > 0) {
+            throw new GracefulResponseException("库存告警阈值不能大于物料剩余库存!");
+        }
+
+        if (form.getCost().compareTo(form.getPrice()) > 0) {
+            throw new GracefulResponseException("物料成本不能大于销售价格!");
+        }
+
+
+        Material material = new Material();
+
+        try {
+            BeanUtil.copyProperties(form, material);
+        } catch (ConvertException e) {
+            throw new GracefulResponseException("物料类型不合法!");
+        }
+
+        if (Boolean.FALSE.equals(imaterialService.save(material))) {
+            throw new ServerErrorException();
+        }
+    }
+
+    @PutMapping(path = "/materialManager/update")
+    @Operation(summary = "库管更新物料信息", description = "库管更新物料信息")
+    public void updateMaterial(@RequestBody @Valid ManngerUpdateMaterialForm form) {
+
+        Material material = imaterialService.getById(form.getMaterialId());
+
+        if (null != material) {
+            //加分布式锁
+            String lockKey = NAMESPACE_4_ORDER_LOCK + form.getMaterialId();
+            RLock fairLock = redissonClient.getFairLock(lockKey);
+            if (fairLock.tryLock()) {
+                try {
+                    if (form.getMaterialName() != null) {
+                        material.setMaterialName(form.getMaterialName());
+                    }
+                    if (form.getMaterialDesc() != null) {
+                        material.setMaterialDesc(form.getMaterialDesc());
+                    }
+                    if (form.getMaterialCoverFileId() != null) {
+                        material.setMaterialCoverFileId(form.getMaterialCoverFileId());
+                    }
+                    if (form.getUnit() != null) {
+                        material.setUnit(form.getUnit());
+                    }
+                    if (form.getStock() != null) {
+                        material.setStock(material.getStock().add(form.getStock()));
+                    }
+                    if (form.getCost() != null) {
+                        material.setCost(form.getCost());
+                    }
+                    if (form.getPrice() != null) {
+                        material.setPrice(form.getPrice());
+                    }
+                    if (form.getAlertNum() != null) {
+                        material.setAlertNum(form.getAlertNum());
+                    }
+
+
+                    if (material.getAlertNum().compareTo(material.getStock()) > 0) {
+                        throw new GracefulResponseException("库存告警阈值不能大于物料剩余库存!");
+                    }
+                    if (material.getCost().compareTo(material.getPrice()) > 0) {
+                        throw new GracefulResponseException("物料成本不能大于销售价格!");
+                    }
+                    if (Boolean.FALSE.equals(imaterialService.updateById(material))) {
+                        throw new ServerErrorException();
+                    }
+                } catch (ConvertException e) {
+                    throw new GracefulResponseException("物料类型不合法!");
+                }
+                fairLock.unlock();
+            }
+
+        } else {
+            throw new GracefulResponseException("库存中不存在该物料!");
+        }
+
+    }
 }
---
+++ src/main/java/com/mi/aftersales/vo/form/ManngerAddMaterialForm.java
@@ -0,0 +1,54 @@
+package com.mi.aftersales.vo.form;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import java.math.BigDecimal;
+
+/**
+ * @author QYenon
+ * @create 2024/5/21
+ */
+@Getter
+@Setter
+@Accessors(chain = true)
+@Schema(title = "库管添加物料",description = "库管添加物料")
+public class ManngerAddMaterialForm {
+
+    @Schema(description = "物料名称")
+    @Length(min = 1, max = 256, message = "")
+    @NotEmpty(message = "物料名称不能为空")
+    private String materialName;
+
+    @Schema(description = "物料富文本描述")
+    private String materialDesc;
+
+    @Schema(description = "物料封面描述图片")
+    private String materialCoverFileId;
+
+    @Schema(description = "计量单位")
+    private String unit;
+
+    @Schema(description = "物料剩余库存")
+    @Min(value = 0, message = "库存数量必须大于等于0")
+    private BigDecimal stock;
+
+    @Schema(description = "物料成本")
+    @Min(value = 0, message = "物料成本必须大于等于0")
+    private BigDecimal cost;
+
+    @Schema(description = "销售价格")
+    @Min(value = 0, message = "销售价格必须大于等于0")
+    private BigDecimal price;
+
+    @Schema(description = "库存告警阈值")
+    @Min(value = 0, message = "库存告警阈值必须大于等于0")
+    private BigDecimal alertNum;
+
+
+}
