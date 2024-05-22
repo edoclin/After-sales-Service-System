f3df54d8a1352dc774dd56b341fc111350cadd5c
fengs <fengsx@yeah.net>
2024-05-22 20:46:56 +0800

    物料模块

--- src/main/java/com/mi/aftersales/controller/MaterialController.java
+++ src/main/java/com/mi/aftersales/controller/MaterialController.java
@@ -2,23 +2,29 @@ package com.mi.aftersales.controller;
 
 
 import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.convert.ConvertException;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.feiniaojin.gracefulresponse.GracefulResponseException;
 import com.mi.aftersales.entity.Material;
 import com.mi.aftersales.exception.graceful.ServerErrorException;
 import com.mi.aftersales.service.IMaterialService;
-import com.mi.aftersales.vo.form.ManngerAddMaterialForm;
+import com.mi.aftersales.vo.form.MaterialForm;
 import com.mi.aftersales.vo.form.ManngerUpdateMaterialForm;
 import io.swagger.v3.oas.annotations.Operation;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.mi.aftersales.service.IMaterialService.NAMESPACE_4_MATERIAL_LOCK;
 
 /**
  * <p>
@@ -31,8 +37,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/aftersales/material")
 public class MaterialController {
-
-    private static final String NAMESPACE_4_ORDER_LOCK = "materialLock:";
+    private static final Logger log = LoggerFactory.getLogger(MaterialController.class);
 
     @Resource
     private IMaterialService imaterialService;
@@ -40,20 +45,10 @@ public class MaterialController {
     @Resource
     private RedissonClient redissonClient;
 
-    @PostMapping(path = "/materialManager/add")
+    @PostMapping(path = "/")
     @Operation(summary = "库管添加物料", description = "库管添加物料")
-    public void addMaterial(@RequestBody @Valid ManngerAddMaterialForm form) {
-
-        LambdaQueryWrapper<Material> queryWrapper = new LambdaQueryWrapper<>();
-
-        queryWrapper.eq(null != form.getMaterialName(), Material::getMaterialName, form.getMaterialName());
-
-        List<Material> materialList = imaterialService.list(queryWrapper);
-
-        if (null != materialList && materialList.size() > 0) {
-            throw new GracefulResponseException("物料命名重复，请更换物料名!");
-        }
-
+    public void addMaterial(@RequestBody @Valid MaterialForm form) {
+        // update 不用查询，直接save，有联合唯一索引，catch DuplicateKeyException异常
         if (form.getAlertNum().compareTo(form.getStock()) > 0) {
             throw new GracefulResponseException("库存告警阈值不能大于物料剩余库存!");
         }
@@ -62,77 +57,57 @@ public class MaterialController {
             throw new GracefulResponseException("物料成本不能大于销售价格!");
         }
 
-
         Material material = new Material();
 
         try {
             BeanUtil.copyProperties(form, material);
+            if (Boolean.FALSE.equals(imaterialService.save(material))) {
+                throw new ServerErrorException();
+            }
         } catch (ConvertException e) {
             throw new GracefulResponseException("物料类型不合法!");
-        }
-
-        if (Boolean.FALSE.equals(imaterialService.save(material))) {
+        } catch (DuplicateKeyException e) {
+            throw new GracefulResponseException("物料名称重复！");
+        } catch (Exception e) {
             throw new ServerErrorException();
         }
     }
 
-
-    @PutMapping(path = "/materialManager/update")
+    @PutMapping(path = "/")
     @Operation(summary = "库管更新物料信息", description = "库管更新物料信息")
     public void updateMaterial(@RequestBody @Valid ManngerUpdateMaterialForm form) {
-
         Material material = imaterialService.getById(form.getMaterialId());
 
-        if (null != material) {
-            //加分布式锁
-            String lockKey = NAMESPACE_4_ORDER_LOCK + form.getMaterialId();
-            RLock fairLock = redissonClient.getFairLock(lockKey);
-            if (fairLock.tryLock()) {
-                try {
-                    if (form.getMaterialName() != null) {
-                        material.setMaterialName(form.getMaterialName());
-                    }
-                    if (form.getMaterialDesc() != null) {
-                        material.setMaterialDesc(form.getMaterialDesc());
-                    }
-                    if (form.getMaterialCoverFileId() != null) {
-                        material.setMaterialCoverFileId(form.getMaterialCoverFileId());
-                    }
-                    if (form.getUnit() != null) {
-                        material.setUnit(form.getUnit());
-                    }
-                    if (form.getStock() != null) {
-                        material.setStock(material.getStock().add(form.getStock()));
-                    }
-                    if (form.getCost() != null) {
-                        material.setCost(form.getCost());
-                    }
-                    if (form.getPrice() != null) {
-                        material.setPrice(form.getPrice());
-                    }
-                    if (form.getAlertNum() != null) {
-                        material.setAlertNum(form.getAlertNum());
-                    }
+        if (BeanUtil.isEmpty(material)) {
+            throw new GracefulResponseException("非法的物料Id");
+        }
+        //加分布式锁
+        RLock fairLock = null;
+        try {
+            fairLock = redissonClient.getFairLock(NAMESPACE_4_MATERIAL_LOCK + form.getMaterialId());
+            if (fairLock.tryLock(30, TimeUnit.SECONDS)) {
+                // update 直接忽略null值
+                BeanUtil.copyProperties(form, material, CopyOptions.create().ignoreNullValue());
 
-
-                    if (material.getAlertNum().compareTo(material.getStock()) > 0) {
-                        throw new GracefulResponseException("库存告警阈值不能大于物料剩余库存!");
-                    }
-                    if (material.getCost().compareTo(material.getPrice()) > 0) {
-                        throw new GracefulResponseException("物料成本不能大于销售价格!");
-                    }
-                    if (Boolean.FALSE.equals(imaterialService.updateById(material))) {
-                        throw new ServerErrorException();
-                    }
-                } catch (ConvertException e) {
-                    throw new GracefulResponseException("物料类型不合法!");
+                if (material.getAlertNum().compareTo(material.getStock()) > 0) {
+                    throw new GracefulResponseException("库存告警阈值不能大于物料剩余库存!");
                 }
+                if (material.getCost().compareTo(material.getPrice()) > 0) {
+                    throw new GracefulResponseException("物料成本不能大于销售价格!");
+                }
+                if (Boolean.FALSE.equals(imaterialService.updateById(material))) {
+                    throw new ServerErrorException();
+                }
+            }
+        } catch (ConvertException e) {
+            throw new GracefulResponseException("物料类型不合法!");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ServerErrorException();
+        } finally {
+            if (null != fairLock) {
                 fairLock.unlock();
             }
-
-        } else {
-            throw new GracefulResponseException("库存中不存在该物料!");
         }
-
     }
 }
