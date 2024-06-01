006b6b6619c7f845552b1d959cd3aa7709deb10d
fengs <fengsx@yeah.net>
2024-06-01 23:19:08 +0800

    优化查询待办工单逻辑

--- src/main/java/com/mi/aftersales/exception/graceful/UrlNotFoundException.java
+++ src/main/java/com/mi/aftersales/exception/graceful/UrlNotFoundException.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
  * @description: 接口不存在
  * @return:
  * @author: edoclin
- * @created: 2024/6/1 17:50
+ * @created: 2024/6/1 20:12
  **/
 @ExceptionAliasFor(code = "1", msg = "接口不存在！", aliasFor = NoHandlerFoundException.class)
 public class UrlNotFoundException extends RuntimeException {
