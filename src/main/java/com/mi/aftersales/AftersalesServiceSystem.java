6d51fddca4b1ef645792709ecfad594c8feedd6a
fengs <fengsx@yeah.net>
2024-06-06 22:44:32 +0800

    修复问题 & 更新日志策略

--- src/main/java/com/mi/aftersales/AftersalesServiceSystem.java
+++ src/main/java/com/mi/aftersales/AftersalesServiceSystem.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableGracefulResponse
 @SpringBootApplication
 @MapperScan("com.mi.aftersales.mapper")
-
 public class AftersalesServiceSystem {
     public static void main(String[] args) {
         SpringApplication.run(AftersalesServiceSystem.class, args);
